93b58e39cf50f32bff2aa24d9fde3a61c2ac1465
EduAspIng <eespinoza@edu.upolitecnica.cr>
2023-09-27 10:17:52 -0600

    Agregar archivos de proyecto.

---
+++ Prueba para git/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_para_git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //La prueba esta aqui
+        }
+    }
+}
